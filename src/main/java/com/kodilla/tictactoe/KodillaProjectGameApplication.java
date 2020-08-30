4fc56c05cc76b49a3794bb56692b4ff41c56bf82
Kamil Józefowski <k.m.jozefowski@gmail.com>
2020-08-30 19:27:54 +0200

    First Commit

---
+++ src/main/java/com/kodilla/tictactoe/KodillaProjectGameApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaProjectGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaProjectGameApplication.class, args);
+    }
+
+}
